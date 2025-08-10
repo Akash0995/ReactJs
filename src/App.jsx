82ca96831dc952b67338a731cafc1912c1c4913d
Akash0995 <mostafa.jbp01@gmail.com>
2025-08-10 14:30:56 +0600

    css using module

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Card from "./Card";
+import Button from "./Button/Button.jsx";
 function App() {
   return(
     <>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Button/>
     </>
   )
   ;
---
+++ src/Button/Button.jsx
@@ -0,0 +1,10 @@
+import style from './Button.module.css'
+function Button()
+{
+    return (
+    <button className={style.buttonst}>
+        Click Me
+    </button>
+);
+}
+export default Button
