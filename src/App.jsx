534ea07d8dcb91d7f51a9d9ee7a411aaddafe4ed
Akash0995 <mostafa.jbp01@gmail.com>
2025-08-18 10:52:30 +0600

    contional filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 //import UserGrating from "./UserGrsting.jsx";
 import List from "./List.jsx";
 function App() {
+  const fruits = [{id:1,name:"Apple",calories:95} ,
+                    {id:2,name:"Orange",calories: 45},
+                    {id:3,name:"Banana",calories: 105} ,
+                    {id:4,name:"Coconut",calories:159} ,
+                    {id:5,name:"Pinaple",calories:37} ];
+    const vegetables = [{id:1,name:"Potato",calories:250} ,
+                    {id:2,name:"Carrots",calories:   450},
+                    {id:3,name:"Broccoli",calories:  320} ,
+                    {id:4,name:"Celery",calories:    265} ,
+                    {id:5,name:"Mushroom",calories:  150} ];
   return(
     <>
-    <List></List>
+    {fruits.length > 0 ?<List items={fruits} category = "Fruits"></List>:null }
+    
+    <List items={vegetables} category = "Vegetables"></List>
     </>
-  )
-  ;
+  )  ;
   
   }
 
--- src/List.jsx
+++ src/List.jsx
@@ -1,17 +1,17 @@
-function List()
+function List(props)
 {
-    const fruits = [{id:1,name:"Apple",calories:95} ,
-                    {id:2,name:"Orange",calories: 45},
-                    {id:3,name:"Banana",calories: 105} ,
-                    {id:4,name:"Coconut",calories:159} ,
-                    {id:5,name:"Pinaple",calories:37} ];
-    fruits.sort((a,b) => b.calories - a.calories);
-    const lowCalFruits = fruits.filter(fruits => fruits.calories < 100);
 
-    const listItem = lowCalFruits.map(lowCalFruits => <li key = {lowCalFruits.id}>
-                                  {lowCalFruits.name}: &nbsp;
-                               <b>{lowCalFruits.calories}</b></li> );
+    const itemList = props.items ;
+    const categories = props.category;
+    const listItem = itemList.map(item => <li key = {item.id}>
+                                  {item.name}: &nbsp;
+                               <b>{item.calories}</b></li> );
 
-    return (<ol>{listItem}</ol>);
+    return (
+        <>
+            <h3 className="list-category">{categories}</h3>
+            <ol className="list-item">{listItem}</ol>
+        </>
+);
 }
 export default List 
