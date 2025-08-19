091f6cd88b1d8ad2d1e8733ccc9cbcf79afa167e
Akash0995 <mostafa.jbp01@gmail.com>
2025-08-19 12:36:55 +0600

    image onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 //import UserGrating from "./UserGrsting.jsx";
 import Button from "./Button.jsx";
 import List from "./List.jsx";
+import ProfilePicture from "./ProfilePicture.jsx"
 function App() {
 
   return(
     <>
-     <Button></Button>
+     <ProfilePicture></ProfilePicture>
     </>
   )  ;
   
