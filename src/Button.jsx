a027121a3d8ae36cf43283f0e51d7a1c03640aa1
Akash0995 <mostafa.jbp01@gmail.com>
2025-08-19 10:48:07 +0600

    addition of onclick event

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,19 +1,26 @@
 
 function Button()
 {
- const styles =   {
-    backgroundColor:"hsl(200, 100%, 50%)" ,
-    color: "white",
-    borderRadius: "5px",
-    padding:"10px 20px" ,
-    border: "none",
-    cursor:"pointer" ,
-    
- };
+    let count = 0;
+    const handleClick = (name) => 
+    {
+        if(count<3)
+        {
+            count++;
+            console.log(`${name} you clicked me ${count} times` );
+        }
+        else
+        {
+            console.log(`${name} stop clicking me!`);
+        }
+    }
+    //console.log("Akash");
+    //const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+
 
     return (
-    <button style={styles}>
-        Click Me
+    <button onClick={ () => handleClick("Bro")}>
+       Click Me 😁  
     </button>
 );
 }
