6ccf1eb2138d832a4c5947bb72c6f933f20afa49
Akash0995 <mostafa.jbp01@gmail.com>
2025-08-25 12:53:56 +0600

    color picker addition

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import List from "./List.jsx";
 import ProfilePicture from "./ProfilePicture.jsx"
 import MyComponent from "./MyComponent.jsx";
 import Counter from "./Counter.jsx";
+import ColorPicker from "./ColorPicker.jsx";
 function App() {
 
   return(
     <>
-     <MyComponent></MyComponent>
+     <ColorPicker></ColorPicker>
     </>
   )  ;
   
