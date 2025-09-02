87aa6d8641d61a08c4a036c97efee0f5203039b2
Akash0995 <mostafa.jbp01@gmail.com>
2025-09-02 11:26:14 +0600

    add and remove elements

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import MyComponent from "./MyComponent.jsx";
 import Counter from "./Counter.jsx";
 import ColorPicker from "./ColorPicker.jsx";
 import ObComponent from "./ObComponent.jsx";
+import MyFoodComponent from "./MyFoodComponent.jsx";
 function App() {
 
   return(
     <>
-     <ObComponent></ObComponent>
+     <MyFoodComponent></MyFoodComponent>
     </>
   )  ;
   
